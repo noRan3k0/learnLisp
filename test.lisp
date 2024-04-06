663d8e63eb6f79d7157e646ff1280fb66db1652c
HTmake-code <fi1er01hkr10ty@gmail.com>
2024-04-06 17:05:54 +0900

    2章：setfで変数の値更新

--- test.lisp
+++ test.lisp
@@ -2,6 +2,5 @@
     ((firstNum 1)
     (secondNum 2))                  ; (())は変数宣言、letを囲む()の間だけで有効。                 
     (print (+ firstNum secondNum))  ; 表示
-    (let 
-        ((nextNum (+ firstNum secondNum)))  ; 変数宣言
-        (print (+ nextNum secondNum))))     ; 表示
+    (setf firstNum (+ firstNum secondNum))      ; 変数の値の更新
+    (print (+ firstNum secondNum)))  ; 表示
