44e02455da407bd7ae270d3d49d1be30825508a5
HTmake-code <fi1er01hkr10ty@gmail.com>
2024-04-08 14:06:27 +0900

    あきてきた。

--- test.lisp
+++ test.lisp
@@ -15,3 +15,12 @@
 ;; これらの組み合わせで、リスト内の対象要素にアクセス。
 (print (first (rest '(a1 b2 c3))))          ; b2にアクセス
 (print (first (rest (rest '(a1 b2 c3)))))   ; c3にアクセス
+
+
+;; リストへの要素の追加
+(print "リストへの要素追加")
+(print (cons 0.1 '(2 -3 0.444)))
+(print (cons "string" '("float" "int")))
+(print (cons '(a1 b2) '(c3 d4 e5)))
+(print (cons () "a"))
+(print (cons "a" nil))
